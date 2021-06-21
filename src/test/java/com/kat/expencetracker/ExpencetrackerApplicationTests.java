84ad7ebf18333800895ef4e5cc19be51cf8cdb9b
Katlego Mosweu <mosweukatlego.km@gmail.com.local>
2021-06-21 15:55:38 +0200

    create project structure with spring init, added project dependacies

---
+++ src/test/java/com/kat/expencetracker/ExpencetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kat.expencetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpencetrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
